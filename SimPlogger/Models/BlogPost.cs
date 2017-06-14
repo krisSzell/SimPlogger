464f3dd2715e8802b8e5acb83a38c014af0de6e5
Krzysztof Skorupa <skorupa.krzysztof1@gmail.com>
2017-06-14 13:55:52 +0200

    Init

---
+++ SimPlogger/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimPlogger.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CtaHeader { get; set; }
+        public byte CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
